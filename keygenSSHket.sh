6a8f7c0d5d28134bf0e325e8045506b7359445dc
MAXPOL <maxrol32@gmail.com>
2021-09-03 22:30:23 +0300

    Create keygenSSHket.sh

---
+++ keygenSSHket.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo "y" | ssh-keygen -q -t rsa -b 4096 -P ""  -f ~/.ssh/id_rsa --stdin
+ssh-copy-id -f ceph@192.168.0.106
+ssh-copy-id -f ceph@192.168.0.107
+ssh-copy-id -f ceph@192.168.0.108
+ssh-copy-id -f ceph@192.168.0.109
